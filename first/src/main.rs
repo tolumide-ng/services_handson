dcb7a99586822484b11c494f826959ba7d2a695a
tolumide-ng <tolumideshopein@gmail.com>
2022-06-19 01:24:51 +0100

    Implement service function

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,16 +1,37 @@
 use std::net::SocketAddr;
 
-use hyper::{server::Server, service::service_fn_ok, Response, Body, rt::{Future, run}};
+use hyper::{server::Server, service::{service_fn_ok, service_fn}, Response, Body, rt::{run}, Request, http::Error, Method, StatusCode};
+use futures::{future, Future};
+
+const INDEX: &'static str = r#"
+<!doctype html>
+<html>
+    <head>
+        <title>Rust Microservice</title>
+    </head>
+    <body>
+        <h3>Rust Microservice
+    </body>
+</html>
+"#;
+
+fn microservice_handler(req: Request<Body>) -> impl Future<Item = Response<Body>, Error=Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            future::ok(Response::new(INDEX.into()))
+        },
+        _ => {
+            let response = Response::builder().status(StatusCode::NOT_FOUND).body(Body::empty()).unwrap();
+            future::ok(response)
+        }
+    }
+}
 
 fn main() {
  let addr: SocketAddr = ([127, 0, 0, 1], 8080).into();
  let builder = Server::bind(&addr);
 
- let server = builder.serve(|| {
-    service_fn_ok(|_| {
-        Response::new(Body::from("Almost microservice..."))
-    })
- });
+ let server = builder.serve(|| service_fn(microservice_handler));
 
  let server = server.map_err(drop);
  run(server);
