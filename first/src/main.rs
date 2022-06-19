6ee66dac8a68fb7965bd5aec7210f3256ac1faf6
tolumide-ng <tolumideshopein@gmail.com>
2022-06-19 02:10:01 +0100

    Shared state

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,7 +1,12 @@
-use std::net::SocketAddr;
-
-use hyper::{server::Server, service::{service_fn_ok, service_fn}, Response, Body, rt::{run}, Request, http::Error, Method, StatusCode};
+use std::{net::SocketAddr, sync::{Arc, Mutex}, fmt};
 use futures::{future, Future};
+use hyper::{server::Server, Response, Body, rt::{run}, Request, http::Error, Method, StatusCode};
+use hyper::service::service_fn;
+use slab::Slab;
+
+type UserId = u64;
+struct UserData;
+type UserDb = Arc<Mutex<Slab<UserData>>>;
 
 const INDEX: &'static str = r#"
 <!doctype html>
@@ -15,7 +20,7 @@ const INDEX: &'static str = r#"
 </html>
 "#;
 
-fn microservice_handler(req: Request<Body>) -> impl Future<Item = Response<Body>, Error=Error> {
+fn microservice_handler(req: Request<Body>, user_db: &UserDb) -> impl Future<Item = Response<Body>, Error=Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
             future::ok(Response::new(INDEX.into()))
@@ -30,8 +35,12 @@ fn microservice_handler(req: Request<Body>) -> impl Future<Item = Response<Body>
 fn main() {
  let addr: SocketAddr = ([127, 0, 0, 1], 8080).into();
  let builder = Server::bind(&addr);
+ let user_db = Arc::new(Mutex::new(Slab::new()));
 
- let server = builder.serve(|| service_fn(microservice_handler));
+ let server = builder.serve(move|| {
+    let user_db = user_db.clone();
+    service_fn(move |req| microservice_handler(req, &user_db))
+});
 
  let server = server.map_err(drop);
  run(server);
